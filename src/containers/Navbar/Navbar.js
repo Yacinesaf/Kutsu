e9d2c6154d8d164f48c86649806a8d11db306b48
Yacinesaf <yacinesaf96@gmail.com>
2020-11-30 17:29:07 -0500

    installed framer motion and navbar is done

--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -1,5 +1,33 @@
 import React, { useState } from 'react';
 import './style.css'
+import { motion } from 'framer-motion'
+
+const variants = {
+  open: { height: '100vh' },
+  closed: { height: 64, transition: { damping: 200, delay: 0.4 }, overflow: 'hidden' }
+}
+const sectionVariants = {
+  open: { x: 0, opacity: 1, y: 100 },
+  closed: { x: -20, opacity: 0, y: 100 },
+}
+const sectionsVariants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDirection: 2,
+      delayChildren: 0.2,
+      when: 'afterChildren'
+    }
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.1,
+      staggerDirection: 1,
+    }
+  }
+}
+
+const navSections = ['Home', 'About', 'Contact', 'Sign up/Log in']
 
 function Navbar({ smDown }) {
   const styles = {
@@ -33,20 +61,47 @@ function Navbar({ smDown }) {
       position: 'absolute',
       right: -8,
       top: 0
-    }
+    },
+    cartCounterMobile: {
+      backgroundColor: 'black',
+      color: 'white',
+      padding: 5,
+      borderRadius: '100%',
+      height: 20,
+      width: 20,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: 10,
+      position: 'absolute',
+      right: 12,
+      top: -5
+    },
   }
   const [cartCounter, setCartCounter] = useState(0)
   const [menuIsClicked, setMenuIsClicked] = useState(false)
 
   return (
-    <div>
-      <div className="row mx-0 justify-content-center" style={styles.navbar}>
-        <div className="col-md-11 px-0" >
-          <div className="row justify-content-between align-items-center mx-0">
-            <div style={styles.navTtitle}>
-              Kutsu
+    <motion.div
+      variants={variants}
+      initial='closed'
+      animate={menuIsClicked ? 'open' : 'closed'}
+      transition={{ damping: 400, duration: 0.4 }}
+      className="row mx-0 justify-content-center"
+      style={styles.navbar}>
+      <div className="col-md-11 px-0" >
+        <div className="row justify-content-between align-items-center mx-0">
+          <div style={styles.navTtitle}>
+            Kutsu
           </div>
-            {smDown ?
+          {smDown ?
+            <div style={{display : 'flex', alignItems : 'center', }}>
+              <div style={{ paddingRight: 20, position: 'relative' }}>
+                <div style={styles.cartCounterMobile}>{cartCounter}</div>
+                <svg width="1.75em" height="1.75em" viewBox="0 0 16 16" className="bi bi-cart2" fill="black" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                </svg>
+              </div>
               <button
                 style={{ paddingTop: 5 }}
                 onClick={() => { setMenuIsClicked(!menuIsClicked) }}
@@ -56,48 +111,51 @@ function Navbar({ smDown }) {
                   <span className="hamburger-inner" />
                 </span>
               </button>
-              :
-              <div className='d-flex align-items-center'>
-                <div style={styles.navText}>
-                  About
             </div>
-                <div style={styles.navText}>
-                  Home
+
+
+            :
+            <div className='d-flex align-items-center'>
+              <div style={styles.navText}>
+                About
             </div>
-                <div style={styles.navText}>
-                  Contact
+              <div style={styles.navText}>
+                Home
             </div>
-                <div style={styles.navText}>
-                  Login/Sign up
+              <div style={styles.navText}>
+                Contact
             </div>
-                <div style={{ paddingLeft: 20, paddingTop: 5, position: 'relative' }}>
-                  <div style={styles.cartCounter}>{cartCounter}</div>
-                  <svg width="1.75em" height="1.75em" viewBox="0 0 16 16" className="bi bi-cart2" fill="black" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
-                  </svg>
-                </div>
+              <div style={styles.navText}>
+                Login/Sign up
+            </div>
+              <div style={{ paddingLeft: 20, paddingTop: 5, position: 'relative' }}>
+                <div style={styles.cartCounter}>{cartCounter}</div>
+                <svg width="1.75em" height="1.75em" viewBox="0 0 16 16" className="bi bi-cart2" fill="black" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                </svg>
               </div>
-            }
-          </div>
+            </div>
+          }
         </div>
+        <motion.div initial='closed'
+          animate={menuIsClicked ? 'open' : 'closed'} variants={sectionsVariants}>
+          {smDown && (
+            navSections.map(section => (
+              <motion.div
+                onClick={() => {
+                  setMenuIsClicked(false)
+                }}
+                className='py-3'
+                key={section}
+                variants={sectionVariants}
+                style={{ textDecoration: 'none', color: 'black', fontSize: '1.35rem', fontWeight: 600, textAlign: 'center' }}>
+                {section}
+              </motion.div>
+            ))
+          )}
+        </motion.div>
       </div>
-      {smDown ?
-        <div className={`d-flex flex-column align-items-center w-100 py-4 ${menuIsClicked ? 'menuDropped' : 'menu'}`}>
-          <div className='py-1' style={styles.navText}>
-            About
-        </div>
-          <div className='py-1' style={styles.navText}>
-            Home
-        </div>
-          <div className='py-1' style={styles.navText}>
-            Contact
-        </div>
-          <div className='py-1' style={styles.navText}>
-            Login/Sign up
-        </div>
-        </div>
-        : null}
-    </div>
+    </motion.div>
   );
 }
 
